99b07282b8851ba9d419bc9800d5ed6599b1c1cf
Vivek <vs690799@gmail.com>
2024-05-22 19:44:23 +0530

    updated upload/route.ts

--- app/api/friends/upload/route.ts
+++ app/api/friends/upload/route.ts
@@ -1,3 +1,4 @@
+
 import { auth } from "@/app/lib/auth";
 import cloudinary from "@/app/lib/cloudinary";
 import { db } from "@/app/lib/db";
@@ -8,9 +9,8 @@ import { Message, messageValidator } from "@/app/lib/validations/message";
 import fileUploadSchema from "@/app/lib/validations/uploadFile";
 import { nanoid } from "nanoid";
 
-
 export async function POST(req: Request) {
-    const body = await req.json()
+    const body = await req.json();
 
     const session = await auth();
 
@@ -18,23 +18,15 @@ export async function POST(req: Request) {
         return new Response("Unauthorized", { status: 401 });
     }
 
-
-    const validationRes = fileUploadSchema.safeParse(body)
+    const validationRes = fileUploadSchema.safeParse(body);
 
     if (!validationRes.success) {
-        const errorMessage = validationRes.error.errors.map(err => err.message).join(',')
-        return new Response(`Invalid request: ${errorMessage}`, { status: 400 })
-
+        const errorMessage = validationRes.error.errors.map(err => err.message).join(',');
+        return new Response(`Invalid request: ${errorMessage}`, { status: 400 });
     }
 
-    const { file, chatId, type } = validationRes.data
-
-
-
-
+    const { file, chatId, type } = validationRes.data;
     const timestamp = Date.now();
-
-
     const [userId1, userId2] = chatId.split("--");
 
     if (session.user.id !== userId1 && session.user.id !== userId2) {
@@ -42,81 +34,69 @@ export async function POST(req: Request) {
     }
 
     const friendId = session.user.id === userId1 ? userId2 : userId1;
-
-
-    const rawSender = (await fetchRedis(
-        "get",
-        `user:${session.user.id}`,
-    )) as string;
-
+    const rawSender = (await fetchRedis("get", `user:${session.user.id}`)) as string;
     const sender = JSON.parse(rawSender) as User;
 
-
-
     try {
-        await cloudinary.uploader.upload(file, { resource_type: 'auto' },
-            async (error: any, result: any) => {
-                let messageData: Message;
-                if (type.startsWith("image")) {
-
-                    messageData = {
-                        id: nanoid(),
-                        senderId: session.user.id,
-                        image: result.secure_url,
-                        type: type,
-                        timestamp,
-                    };
-                } else if (type.startsWith("video")) {
-                    messageData = {
-                        id: nanoid(),
-                        senderId: session.user.id,
-                        video: result.secure_url,
-                        type: type,
-                        timestamp,
-                    };
-                } else {
-                    // Handle other types or throw an error if not supported
-                    throw new Error('Unsupported message type');
-                }
-
-
-                const validMessage = messageValidator.safeParse(messageData!);
-
-
-                if (validMessage.success) {
-                    console.log('success')
-                    const message = validMessage.data as Message
-
-                    await triggerPusherEvent(toPusherKey(`chat:${chatId}`), "incoming-message", message);
-                    await triggerPusherEvent(toPusherKey(`user:${friendId}:chats`), "new_message", {
-                        ...message,
-                        senderImg: sender.image,
-                        senderName: sender.name,
-                    });
-
-                    await db.zadd(`chat:${chatId}:messages`, {
-                        score: timestamp,
-                        member: JSON.stringify(message),
-                    });
-
-                } else {
-                    throw new Error('Parsing failed.')
-                }
-
-
-            }
-        )
-    } catch (error) {
-        console.log('upload failed:', error)
-        if (error instanceof Error) {
-            return new Response(error.message, {
-                status: 500,
+        const result = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(file, { resource_type: 'auto' }, (error: any, result: any) => {
+                if (error) return reject(error);
+                resolve(result);
             });
+        });
+
+        let messageData: Message;
+        const { secure_url }: { secure_url: string } = result as any
+
+
+        if (type.startsWith("image")) {
+            messageData = {
+                id: nanoid(),
+                senderId: session.user.id,
+                image: secure_url,
+                type: type,
+                timestamp,
+            };
+        } else if (type.startsWith("video")) {
+            messageData = {
+                id: nanoid(),
+                senderId: session.user.id,
+                video: secure_url,
+                type: type,
+                timestamp,
+            };
+        } else {
+            throw new Error('Unsupported message type');
         }
-        return new Response('failed to send', { status: 500 })
+
+        const validMessage = messageValidator.safeParse(messageData);
+
+        if (validMessage.success) {
+            const message = validMessage.data as Message;
+
+            await triggerPusherEvent(toPusherKey(`chat:${chatId}`), "incoming-message", message);
+            await triggerPusherEvent(toPusherKey(`user:${friendId}:chats`), "new_message", {
+                ...message,
+                senderImg: sender.image,
+                senderName: sender.name,
+            });
+
+            await db.zadd(`chat:${chatId}:messages`, {
+                score: timestamp,
+                member: JSON.stringify(message),
+            });
+
+        } else {
+            throw new Error('Parsing failed.');
+        }
+    } catch (error) {
+        console.log('upload failed:', error);
+        if (error instanceof Error) {
+            return new Response(error.message, { status: 500 });
+        }
+        return new Response('failed to send', { status: 500 });
     }
 
-
-    return new Response('OK')
-
+    return new Response('OK');
 }
+
