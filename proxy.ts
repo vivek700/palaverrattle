5c7411aeb1a0f56890db089053174f0906e0242f
Vivek <vivek.etc@proton.me>
2026-05-08 04:13:25 +0530

    add proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,33 @@
+import { auth } from "./app/lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth(async (req) => {
+  const pathname = req.nextUrl.pathname;
+
+  const isLoggedIn = !!req.auth;
+  const isLoginPage = pathname.startsWith("/login");
+
+  const sensitiveRoutes = ["/dashboard"];
+
+  const isAccessingSesnsitiveRoute = sensitiveRoutes.some((route) =>
+    pathname.startsWith(route),
+  );
+
+  if (isLoginPage) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+    return NextResponse.next();
+  }
+  if (!isLoggedIn && isAccessingSesnsitiveRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+});
+
+export const config = {
+  matcher: ["/", "/login", "/dashboard/:path*"],
+};
